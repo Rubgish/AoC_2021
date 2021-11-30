245dc05f685adf0801410b84b7c33365e57f00a7
Rubgish <mattjparkes@hotmail.com>
2021-11-30 18:12:19 +0000

    Start like you mean to go on - with the jankiest bash template you can manage in 3 minutes

---
+++ template.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+day="day$1"
+day_a="${day}a.cpp"
+day_b="${day}b.cpp"
+day_input="${day}_input"
+echo $day
+echo $day_b
+
+cp template.cpp $day_a
+cp template.cpp $day_b
+cp input_template $day_input
+
+sed -i "s/DAYNNN/${day}/g" $day_a
+sed -i "s/DAYNNN/${day}/g" $day_b
